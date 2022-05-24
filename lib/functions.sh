1fc500611ff597db32157d092f817f1841fcc904
Chris Cheetham <chris@cheetham.com>
2022-05-24 11:47:26 -0400

    add an err message

--- lib/functions.sh
+++ lib/functions.sh
@@ -4,15 +4,18 @@ cli_yellow="tput setaf 3"
 cli_reset="tput sgr0"
 
 die () {
-  $cli_red
-  echo $*
-  $cli_reset
-  tput sgr0
+  red $*
   exit 1
 }
 
 msg () {
   $cli_green
+  echo $*
+  $cli_reset
+}
+
+red () {
+  $cli_red
   echo $*
   $cli_reset
 }
